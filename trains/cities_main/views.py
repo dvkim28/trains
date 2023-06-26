25fab48db07c4271330bdcbd12dee2d74db5accd
dvkim28 <d.villarionovich@gmail.com>
2023-06-26 17:11:05 +0300

    added table, and update city

--- trains/cities_main/views.py
+++ trains/cities_main/views.py
@@ -1,13 +1,26 @@
 from urllib import request
 
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DeleteView, CreateView, UpdateView
 
+from .forms import CityForm
 from .models import City
 
 
 class TrainListView(ListView):
     model = City
-    paginate_by = 5
     context_object_name = 'city'
     template_name = 'cities_main/index.html'
+
+class CityCreateView(CreateView):
+    model = City
+    template_name = 'cities_main/includes/create_form.html'
+    form_class = CityForm
+    success_url = reverse_lazy('homepage')
+
+class CityUpdateView(UpdateView):
+    model = City
+    template_name = 'cities_main/update_city.html'
+    success_url = reverse_lazy('homepage')
+    form_class = CityForm
